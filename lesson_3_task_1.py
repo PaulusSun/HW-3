e2ad047ea55702b4be97c874dcd8000d8e29de69
PaulusSun <123460451+PaulusSun@users.noreply.github.com>
2023-02-03 20:56:18 +0300

    Add files via upload

--- lesson_3_task_1.py
+++ lesson_3_task_1.py
@@ -1,3 +1,9 @@
 from user import User
 
 my_user = User('Pavel', 'Brovkin')
+
+my_user.printer_first_name()
+
+my_user.printer_last_name()
+
+my_user.printer_first_last_name()
--- user.py
+++ user.py
@@ -1,5 +1,10 @@
 class User:
     def __init__(self, first_name, last_name):
-        print("Имя: "+str(first_name))
-        print("Фамилия: "+str(last_name))
-        print("Имя и Фамилия: "+str(first_name)+' '+str(last_name))
+        self.first_name = first_name
+        self.last_name = last_name   
+    def printer_first_name(self):
+        print("Имя: ", self.first_name)
+    def printer_last_name(self):
+        print("Фамилия: ", self.last_name)
+    def printer_first_last_name(self):
+        print("Имя и Фамилия: ", self.first_name, self.last_name)
